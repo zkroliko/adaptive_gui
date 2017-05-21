965862e52017a96d0999caac81f9702e31517644
zkroliko <zbkrolikowski@gmail.com>
2017-05-21 16:24:59 +0200

    Refactor

--- gui.py
+++ gui.py
@@ -10,28 +10,24 @@ import math
 
 
 class Application(Frame):
-
-    FRAMERATE = 20
-    c = None
+    FRAME_RATE = 20
+    BASE_SIZE = 50
 
     RATIO_MIN = 0.2
     RATIO_MAX = 5.0
 
-    queue = Queue(FRAMERATE)
-
     req = urllib2.Request(url="http://localhost:8080")
 
     def update(self):
-        ratio = min(self.RATIO_MAX,max(self.RATIO_MIN,float(urllib2.urlopen(self.req).read())))
+        ratio = min(self.RATIO_MAX, max(self.RATIO_MIN, float(urllib2.urlopen(self.req).read())))
         print(ratio)
         self.queue.put_nowait(ratio)
-        # self.update_graphics(ratio)
 
     # This method is run only by the main thread
     def process_input_queue(self):
         if not self.queue.empty():
             ratio = self.queue.get_nowait()
-            size = int(50*ratio)
+            size = int(self.BASE_SIZE * ratio)
             self.c.delete("all")
             self.c.create_rectangle(0, 0, size, size, fill="blue")
             root.update()
@@ -40,14 +36,16 @@ class Application(Frame):
         Frame.__init__(self, master)
 
         self.pack()
+        self.queue = Queue(self.FRAME_RATE)
         self.c = Canvas(root)
         self.c.pack()
 
         while True:
-            thread.start_new_thread(self.update,())
-            sleep(1.0 / self.FRAMERATE)
+            thread.start_new_thread(self.update, ())
+            sleep(1.0 / self.FRAME_RATE)
             self.process_input_queue()
         self.update()
+
 
 root = Tk()
 root.minsize(width=500, height=500)
