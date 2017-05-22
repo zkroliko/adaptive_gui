8712aed95d6d8bc49a2977cbc4cf1c9f90ea3e05
zkroliko <zbkrolikowski@gmail.com>
2017-05-22 09:55:50 +0200

    Add rudimentary smoothing, Change front to better show the idea

--- gui.py
+++ gui.py
@@ -9,14 +9,21 @@ class Application(Frame):
     FRAME_RATE = 20
     BASE_SIZE = 50
 
+    CANVAS_SIZE = 600
+    CANVAS_MIDDLE = CANVAS_SIZE / 2
+
     RATIO_MIN = 0.2
-    RATIO_MAX = 5.0
+    RATIO_DEFAULT = 1.0
+    RATIO_MAX = 8.0
+
+    RATIO_RETENTION = 0.4
 
     req = urllib2.Request(url="http://localhost:8080")
 
     def retrieve_state(self):
         try:
-            ratio = min(self.RATIO_MAX, max(self.RATIO_MIN, float(urllib2.urlopen(self.req).read())))
+            retrieved_data = urllib2.urlopen(self.req).read()
+            ratio = min(self.RATIO_MAX, max(self.RATIO_MIN, float(retrieved_data)))
             print(ratio)
             self.queue.put_nowait(ratio)
         except urllib2.URLError:
@@ -25,18 +32,29 @@ class Application(Frame):
     # This method is run only by the main thread
     def process_input_queue(self):
         if not self.queue.empty():
-            ratio = self.queue.get_nowait()
-            size = int(self.BASE_SIZE * ratio)
+            received_ratio = self.queue.get_nowait()
+            smooth_ratio = self.calculate_ratio(received_ratio)
+            size = int(self.BASE_SIZE * smooth_ratio)
             self.c.delete("all")
-            self.c.create_rectangle(0, 0, size, size, fill="blue")
+            sx, sy, = self.CANVAS_MIDDLE - size, self.CANVAS_MIDDLE - size
+            ex, ey = self.CANVAS_MIDDLE + size, self.CANVAS_MIDDLE + size
+            self.c.create_rectangle(sx, sy, ex, ey, fill="blue")
             root.update()
+
+    def calculate_ratio(self, new_ratio):
+        ratio = (self.old_ratio * self.RATIO_RETENTION + new_ratio) / 2.0
+        self.old_ratio = ratio
+        return ratio
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
 
+        # For "moving average"
+        self.old_ratio = self.RATIO_DEFAULT
+
         self.pack()
         self.queue = Queue(self.FRAME_RATE)
-        self.c = Canvas(root)
+        self.c = Canvas(root, width=600, height=600)
         self.c.pack()
 
         while True:
