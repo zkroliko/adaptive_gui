d243a2829290e454e0a9fe77bbb6c15464fab191
zkroliko <zbkrolikowski@gmail.com>
2017-05-22 12:11:49 +0200

    Isolate, refactor gui

--- clients/simple_gui.py
+++ clients/simple_gui.py
@@ -1,8 +1,10 @@
 import thread
 import urllib2
+from Canvas import Rectangle
 from Tkinter import *
 from multiprocessing import Queue
 from time import sleep
+from PIL import Image
 
 
 class Application(Frame):
@@ -34,12 +36,12 @@ class Application(Frame):
         if not self.queue.empty():
             received_ratio = self.queue.get_nowait()
             smooth_ratio = self.calculate_ratio(received_ratio)
-            size = int(self.BASE_SIZE * smooth_ratio)
-            self.c.delete("all")
-            sx, sy, = self.CANVAS_MIDDLE - size, self.CANVAS_MIDDLE - size
-            ex, ey = self.CANVAS_MIDDLE + size, self.CANVAS_MIDDLE + size
-            self.c.create_rectangle(sx, sy, ex, ey, fill="blue")
+            self.update_rectangle(smooth_ratio)
             root.update()
+
+    def update_rectangle(self,ratio):
+        sx, sy, ex, ey = self.calculate_cords(ratio)
+        self.c.coords(self.rectangle, sx, sy, ex, ey)
 
     def calculate_ratio(self, new_ratio):
         ratio = (self.old_ratio * self.RATIO_RETENTION + new_ratio) / 2.0
@@ -49,23 +51,36 @@ class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
 
+        self.pack()
+
+        # For sync
+        self.queue = Queue(self.FRAME_RATE)
+
         # For "moving average"
         self.old_ratio = self.RATIO_DEFAULT
 
-        self.pack()
-        self.queue = Queue(self.FRAME_RATE)
+        # Canvas
         self.c = Canvas(root, width=600, height=600)
         self.c.pack()
+
+        # Rectangle
+        self.rectangle = self.c.create_rectangle(self.calculate_cords(1.0), fill="blue")
+        root.update()
 
         while True:
             thread.start_new_thread(self.retrieve_state, ())
             sleep(1.0 / self.FRAME_RATE)
             self.process_input_queue()
-        self.retrieve_state()
+
+    def calculate_cords(self, ratio):
+        size = int(self.BASE_SIZE * ratio)
+        sx, sy, = self.CANVAS_MIDDLE - size, self.CANVAS_MIDDLE - size
+        ex, ey = self.CANVAS_MIDDLE + size, self.CANVAS_MIDDLE + size
+        return sx, sy, ex, ey
 
 
 root = Tk()
-root.minsize(width=500, height=500)
+root.minsize(width=600, height=600)
 app = Application(master=root)
 app.mainloop()
 root.destroy()
